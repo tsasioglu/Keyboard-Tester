517846266b823cc9cdb1cb0768ccd47501e98022
tsasioglu <tsasioglu@gmail.com>
2015-03-01 21:13:15 +0000

    Initial Commit

---
+++ KeyboardTester/KeyboardKey.xaml.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows;
+
+namespace KeyboardTester
+{
+    /// <summary>
+    /// Interaction logic for KeyboardKey.xaml
+    /// </summary>
+    public partial class KeyboardKey
+    {
+        public string Text
+        {
+            get { return (string)GetValue(TextProperty); }
+            set { SetValue(TextProperty, value); }
+        }
+        
+        public static readonly DependencyProperty TextProperty =
+            DependencyProperty.Register("Text", typeof(string), typeof(KeyboardKey), new PropertyMetadata(String.Empty));
+
+        
+        public int PaddingOverride
+        {
+            get { return (int)GetValue(PaddingOverrideProperty); }
+            set { SetValue(PaddingOverrideProperty, value); }
+        }
+
+        public static readonly DependencyProperty PaddingOverrideProperty =
+            DependencyProperty.Register("PaddingOverride", typeof(int), typeof(KeyboardKey), new PropertyMetadata(0));
+        
+
+        public bool DefaultPadding
+        {
+            get { return (bool)GetValue(DefaultPaddingProperty); }
+            set { SetValue(DefaultPaddingProperty, value); }
+        }
+
+        public static readonly DependencyProperty DefaultPaddingProperty =
+            DependencyProperty.Register("DefaultPadding", typeof(bool), typeof(KeyboardKey), new PropertyMetadata(true));
+
+
+        public KeyboardKeyModel ViewModel { get; set; }
+        
+        public KeyboardKey()
+        {
+            InitializeComponent();
+            DataContext = this;
+            ViewModel = new KeyboardKeyModel();
+        }
+      
+    }
+
+    public class KeyboardKeyModel : INotifyPropertyChanged
+    {
+        public string Text { get; set; }
+
+        private bool _pressed;
+        public bool Pressed
+        {
+            get { return _pressed; }
+            set
+            {
+                _pressed = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private bool _previouslyPressed;
+        public bool PreviouslyPressed
+        {
+            get { return _previouslyPressed; }
+            set
+            {
+                _previouslyPressed = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null) handler(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ KeyboardTester/MainWindow.xaml.cs
@@ -0,0 +1,130 @@
+﻿using System.Reflection;
+using System.Windows.Input;
+
+namespace KeyboardTester
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void MainWindow_OnKeyDown(object sender, KeyEventArgs e)
+        {
+            var keyControl = GetKeyControl(e);
+            if (keyControl == null)
+                return;
+
+            keyControl.ViewModel.Pressed = true;
+            keyControl.ViewModel.PreviouslyPressed = true;
+
+            if (keyControl == Return)
+            {
+                ReturnLower.ViewModel.Pressed = true;
+                ReturnLower.ViewModel.PreviouslyPressed = true;
+            }
+
+            e.Handled = true;
+        }
+        
+        private void MainWindow_OnKeyUp(object sender, KeyEventArgs e)
+        {
+            var keyControl = GetKeyControl(e);
+            if (keyControl == null)
+                return;
+
+            keyControl.ViewModel.Pressed = false;
+
+            if (keyControl == Return)
+                ReturnLower.ViewModel.Pressed = false;
+
+            if (keyControl == Snapshot)
+                Snapshot.ViewModel.PreviouslyPressed = true;
+
+            e.Handled = true;
+        }
+
+        private KeyboardKey GetKeyControl(KeyEventArgs keyEvent)
+        {
+            if (keyEvent.Key == Key.LeftCtrl && IsModifier(ModifierKeys.Control) && IsNotModifier(ModifierKeys.Alt))
+            {
+                if (keyEvent.IsRepeat)
+                    return null;
+                return LeftCtrl;
+            }
+
+            if (keyEvent.Key == Key.Enter)
+            {
+                if(ReadIsExteneded(keyEvent))
+                    return NumPadEnter;
+                return Return;
+            }
+            
+            if (keyEvent.Key == Key.System)
+            {
+                if (IsModifier(ModifierKeys.Alt))
+                    return LeftAlt;
+                if (keyEvent.SystemKey == Key.F10)
+                    return F10;
+                if (keyEvent.SystemKey == Key.F11)
+                    return F11;
+                if (keyEvent.SystemKey == Key.F12)
+                    return F12;
+                if (ReadRealKey(keyEvent) == Key.LeftAlt)
+                    return LeftAlt;
+                if (ReadRealKey(keyEvent) == Key.RightAlt)
+                    return RightAlt;
+            }
+
+            var keyControl = GetKeyControlNonStandard(keyEvent);
+
+            if (keyControl == null)
+                keyControl = FindName(keyEvent.Key.ToString()) as KeyboardKey;
+
+            return keyControl;
+        }
+
+        private static bool ReadIsExteneded(KeyEventArgs keyEvent)
+        {
+            return (bool)typeof(KeyEventArgs).InvokeMember("IsExtendedKey", BindingFlags.GetProperty | BindingFlags.NonPublic | BindingFlags.Instance, null, keyEvent, null); 
+        }
+
+        private static Key ReadRealKey(KeyEventArgs keyEvent)
+        {
+            return (Key)typeof(KeyEventArgs).InvokeMember("RealKey", BindingFlags.GetProperty | BindingFlags.NonPublic | BindingFlags.Instance, null, keyEvent, null);
+        }
+
+        private static bool IsModifier(ModifierKeys key)
+        {
+            return (Keyboard.Modifiers & key) == key;
+        }
+
+        private static bool IsNotModifier(ModifierKeys key)
+        {
+            return (Keyboard.Modifiers & key) != key;
+        }
+
+        private KeyboardKey GetKeyControlNonStandard(KeyEventArgs keyEvent)
+        {
+            switch (keyEvent.Key)
+            {
+                case Key.Oem1:
+                    return OemSemicolon;
+                case Key.Oem4:
+                    return OemOpenBrackets;
+                case Key.Oem5:
+                    return OemPipe;
+                case Key.Oem6:
+                    return OemCloseBrackets;
+                case Key.Oem7:
+                    return OemQuotes;
+                default:
+                    return null;
+            }
+        }
+    }
+}
